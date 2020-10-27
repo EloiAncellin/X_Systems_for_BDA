6ec6d1fa65029fbda7a8f499036ffbb57d12129f
Nico <nicolas>
2020-10-27 14:39:46 +0100

    Modif in Wrapper class combination

--- X_Systems_for_BDA/src/combination/Combination.java
+++ X_Systems_for_BDA/src/combination/Combination.java
@@ -1,25 +1,20 @@
 package combination;
 
-import java.util.ArrayList;
-
 import utils.LoadData;
 
 public abstract class Combination {
 
-	private String filename;
-	private int lenFile;
 	private Boolean distinct;
 	private double[] keys;
 	private LoadData loadData;
-	private float[] customerPrice;
-	
+
 	public Combination(String filename, int lenFile, Boolean distinct, double[] keys) {
-		this.filename = filename;
-		this.lenFile = lenFile;
 		this.distinct = distinct;
 		this.keys = keys;
 		this.loadData = new LoadData(filename, lenFile);
 	}
+	
+	public abstract void start_combination() throws InterruptedException;
 	
 	public double[] getKeys() {
 		return keys;
@@ -29,7 +24,9 @@ public abstract class Combination {
 		return loadData;
 	}
 
-	public abstract void start_combination() throws InterruptedException;
-	
+	public Boolean getDistinct() {
+		return distinct;
+	}
+
 	
 }
