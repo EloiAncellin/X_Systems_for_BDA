232cfbc3802568800534f7ec7e96cf20c77a8019
julien <julien@localhost>
2020-11-02 20:59:09 +0100

    last fix project sort

--- X_Systems_for_BDA/src/utils/MultiThreadProjectionSort.java
+++ X_Systems_for_BDA/src/utils/MultiThreadProjectionSort.java
@@ -1,6 +1,8 @@
 package utils;
 import java.util.ArrayList;
 import java.util.List;
+
+
 import java.util.Hashtable;
 public class MultiThreadProjectionSort implements Runnable{
 	
@@ -27,13 +29,13 @@ public class MultiThreadProjectionSort implements Runnable{
 		this.projection = Projection.MultiThreadProjectsort(new ArrayList<Integer>(sub),this.cl,this.distinct);
 		this.projection = Projection.removemergeMulti(projection,this.cl);
 		
-	   // System.out.println(projection);
+	  //  System.out.println(projection);
 	}
     
 	public synchronized Hashtable<String, ArrayList<?>> getprojection() {
 		return this.projection;
 	}
-	public static void main(String[] args) {/*
+	public static void main(String[] args) {
 		LoadData Rdata = new LoadData("src/dataset_sorted/dataset_sorted_100.csv",100);
 		Rdata.read();
 		
@@ -49,13 +51,15 @@ public class MultiThreadProjectionSort implements Runnable{
 		ArrayList <Integer> tab2 = new ArrayList<Integer> (tab.subList(0, 50));
 		ArrayList <Integer> tab3 = new ArrayList<Integer> (tab.subList(0, 99));
 	   
-		Thread T1 = new Thread(new MultiThreadProjectionSort(tab2,col,true,cl,All_col_names,1,50));
+		Thread T1 = new Thread(new MultiThreadProjectionSort(tab2,col,false,cl,All_col_names,1,50));
 		T1.start();
 		System.out.println("thread1");
-	    Thread T2 = new Thread(new MultiThreadProjectionSort(tab3,col,true,cl,All_col_names,1,60));
+		
+	    Thread T2 = new Thread(new MultiThreadProjectionSort(tab3,col,false,cl,All_col_names,1,60));
+	//    T2.join();
 	    T2.start();
 	    System.out.println("thread2");
-	 //   System.out.println(getprojection());*/
+	
 	   
 	}
 }
